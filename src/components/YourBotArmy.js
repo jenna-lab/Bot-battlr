12e39c2016569a14faa887de46a046aa5b2a81ec
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-23 12:40:00 +0300

    added map function

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,3 +1,13 @@
 import React from "react";
 const YourBotArmy = ({ army, handleOnClick }) => {
     return (
+         //mapping pver the data for the bots
+    <div className="ui four column grid">
+    <div className="row">
+      {army.map((bot, index) => {
+        return (
+          <div
+            className="card col-2 p-0 m-3 "
+            key={bot.id}
+            onClick={() => handleOnClick(bot.id)}
+          >
