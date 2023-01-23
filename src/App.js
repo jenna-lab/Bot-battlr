f927db168ee3f43f74231674d44d8b39a98cff21
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-23 12:18:14 +0300

    app.js component data

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import { Route,Routes} from 'react-router-dom';
+
+import Army from "./components/Army";
+
+function App() {
+  return (
+    <div>
+      <Army />
+      {/* <Routes>
+        <Route exact path="/" element={<BotCollection/>} />
+        <Route path="./army" element={<YourBotArmy/>} />
+        <Route path="./bot/:id" element={<BotSpecs/>} />
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
