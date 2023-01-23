354b6546d95a1b98f1b9ffdccaf1915ae0fa0af2
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-23 12:33:16 +0300

    added map fucntion

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,3 +1,13 @@
 import React from "react";
 const BotCard = ({ bots, handleOnClick }) => {
     return (
+        <div className="ui four column grid">
+        <div className="row">
+          {bots.map((bot, index) => {
+            return (
+              // passing details about all the bots
+              <div
+                className="card col-2 p-0 m-3 "
+                key={bot.id}
+                onClick={() => handleOnClick(bot.id)}
+              >
