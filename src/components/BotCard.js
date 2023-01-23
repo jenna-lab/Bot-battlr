924f49cd428eb3f9261a0e1996b69e7072de77ea
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-23 12:34:55 +0300

    added footer

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -20,3 +20,30 @@ const BotCard = ({ bots, handleOnClick }) => {
                 <h5 className="card-title">{bot.name}</h5>
                 <p className="card-text">{bot.catchphrase}</p>
               </div>
+              <div className="card-footer">
+                <div className="ui segment">
+                  <div className="ui three column centered grid">
+                    <div className="row">
+                      <div className="column">
+                        <strong>{bot.health}</strong>
+                      </div>
+                      <div className="column col-2">
+                        <i className="icon large circular yellow lightning" />
+                        <strong>{bot.damage}</strong>
+                      </div>
+                      <div className="column">
+                        <i className="icon large circular blue shield" />
+                        <strong>{bot.armor}</strong>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default BotCard;
