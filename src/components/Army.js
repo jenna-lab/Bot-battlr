88b38bc6c2ab5d83b94946f4ead66b3474318748
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-23 12:27:27 +0300

    added usestate and useeffect

--- src/components/Army.js
+++ src/components/Army.js
@@ -1,3 +1,14 @@
 import React, { useState, useEffect } from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
+
+const Army = () => {
+    const [bots, setBots] = useState([]);
+    const [army, setArmy] = useState([]);
+  //prevent page from reloading
+    useEffect(() => {
+      fetch("http://localhost:8001/bots")
+        .then((resp) => resp.json())
+        .then((data) => setBots(data))
+        .catch((err) => console.log(err));
+    }, []);
